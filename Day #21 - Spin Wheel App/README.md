9f20378cbdd0de3e371b132288d0fe96f62b6c12
Victor Martins <vitorchiesa74@gmail.com>
2023-12-22 07:59:53 -0300

    Spin Wheel App

---
+++ Day #21 - Spin Wheel App/README.md
@@ -0,0 +1,10 @@
+# Day #21
+
+### Spin Wheel App
+This JavaScript code creates a spinning roulette application with values associated with certain sectors.❗️
+
+# Screenshot
+Here we have project screenshot :
+
+
+![screenshot](screenshot.jpg)
---
+++ Day #21 - Spin Wheel App/script.js
@@ -0,0 +1,83 @@
+// Selecting DOM elements
+const wheel = document.getElementById("wheel");
+const spinBtn = document.getElementById("spin-btn");
+const finalValue = document.getElementById("final-value");
+
+// Values of min and max angle for each value on the wheel
+const rotationValues = [
+    { minDegree: 0, maxDegree: 30, value: 2 },
+    { minDegree: 31, maxDegree: 90, value: 1 },
+    // ... (similar entries for other values)
+];
+
+// Size of pieces on the wheel
+const data = [16, 16, 16, 16, 16, 16];
+
+// Background color of pieces on the wheel
+var pieColors = ["#1565c0", "#2196f3", "#1565c0", "#2196f3", "#1565c0", "#2196f3"];
+
+// Creating a pie chart using Chart.js
+let myChart = new Chart(wheel, {
+    type: "pie",
+    data: {
+        labels: [1, 2, 3, 4, 5, 6],
+        datasets: [
+            {
+                backgroundColor: pieColors,
+                data: data,
+            },
+        ],
+    },
+    options: {
+        responsive: true,
+        animation: { duration: 0 },
+        plugins: {
+            tooltip: false,
+            legend: { display: false },
+            datalabels: {
+                color: "#ffffff",
+                formatter: (_, context) => context.chart.data.labels[context.dataIndex],
+                font: { size: 24 },
+            },
+        },
+    },
+});
+
+// Function to display the value based on the random angle
+const valueGenerator = (angleValue) => {
+    for (let i of rotationValues) {
+        if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
+            finalValue.innerHTML = `<p>Value: ${i.value}</p>`;
+            spinBtn.disabled = false;
+            break;
+        }
+    }
+};
+
+// Spinner Count and Rotation
+let count = 0;
+let resultValue = 101;
+
+// Start spinning event listener
+spinBtn.addEventListener("click", () => {
+    spinBtn.disabled = true;
+    finalValue.innerHTML = `<p>Good Luck!</p>`;
+    // Generate random degree to stop at
+    let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
+    // Interval for rotation animation
+    let rotationInterval = window.setInterval(() => {
+        myChart.options.rotation = myChart.options.rotation + resultValue;
+        myChart.update();
+        // Reset rotation if it exceeds 360
+        if (myChart.options.rotation >= 360) {
+            count += 1;
+            resultValue -= 5;
+            myChart.options.rotation = 0;
+        } else if (count > 15 && myChart.options.rotation == randomDegree) {
+            valueGenerator(randomDegree);
+            clearInterval(rotationInterval);
+            count = 0;
+            resultValue = 101;
+        }
+    }, 10);
+});
